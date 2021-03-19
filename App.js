b0aee19d37ebcee9bfa294078be5c9e2a8a00e99
AlexiaTdev <alexia.teissier1@epsi.fr>
2021-03-19 10:05:31 +0100

    madame

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Button, StyleSheet, Text, View } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./src/home2.jpg')} style={styles.image}>
+        <Text>GO STYLE!</Text>
+        <Button>Get QRCode</Button>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +24,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  }
 });
+
+/**resizeMode: "cover", 
+ *     
+*/
