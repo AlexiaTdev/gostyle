4ea8e0ca68de6351709fecf86ad65f944578978a
Florian DA CUNHA <florian_dc@hotmail.fr>
2021-03-26 13:05:20 +0100

    liste component getPromo, fonction affichage depuis l'API

--- components/getPromos.js
+++ components/getPromos.js
@@ -1,19 +1,35 @@
 import React from 'react'
+import { Text, View, StyleSheet } from 'react-native';
 
 const GetPromos = ({ promos }) => {
     return (
-    <div>
-        <center><h1>Liste des promos</h1></center>
+    <View style={styles.container}>
+        <Text  style={styles.item}>Liste des promos</Text>
         {promos.map((promo) => (
-        <div class="promo">
-            <div class="promo-body">
-            <h5 class="promo-name">{promo.codePromo}</h5>
-            <p class="promo-info">{promo.reduction}</p>
-            </div>
-        </div>
+        <View style={styles.container}>
+            <Text  style={styles.item}>{promo.codePromo}</Text>
+            <Text  style={styles.item}>{promo.reduction}</Text>
+        </View>
         ))}
-    </div>
+    </View>
     )
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        backgroundColor: 'rgba(247,247,247,1.0)',
+      },
+    })
+  
+    
+
 
 export default GetPromos;
